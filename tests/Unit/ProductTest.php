1e961d1d2ed4e88562515bfa61d4346dd77c60ae
SinemIsolocity <sinem@isolocity.com>
2022-02-18 14:05:25 -0500

    Modify ProductTest

--- tests/Unit/ProductTest.php
+++ tests/Unit/ProductTest.php
@@ -6,16 +6,15 @@ use Tests\TestCase;
 
 class ProductTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
+    public function setup(): void
+    {
+        parent::setUp();
+    }
     public function test_it_stores_new_product()
     {
         $response = $this->post('api/products', [
-            'name' => 'Product 9',
-            'identification' => '00009',
+            'name' => 'Product_9',
+            'identification' => '00011',
             'batch_number' => '0000009',
             'quantity' => '10',
             'price' => '99.9',
@@ -31,9 +30,10 @@ class ProductTest extends TestCase
 
     public function test_it_updates_product()
     {
-        $response = $this->put('api/products/1', [
-            'name' => 'Product update',
-            
+        $response = $this->get('api/products/search/Product_9');
+        $response->assertStatus(200);
+        $response = $this->put('api/products/'.$response->getData()[0]->id, [
+            'name' => 'Product_update',
         ]);
         $response->assertStatus(200);
     
@@ -41,15 +41,28 @@ class ProductTest extends TestCase
     }
     public function test_it_searches_product()
     {
-        $response = $this->get('api/products/search/product');
-        //$response->assertOk();
-        $response->assertStatus(200);
+        $this->json('GET', 'api/products/search/Product_update', ['Accept'=> 'application/json'])
+        ->assertStatus(200)
+        ->assertJson([[
+            'name' => 'Product_update',
+            'identification' => '00011',
+            'batch_number' => '0000009',
+            'quantity' => '10',
+            'price' => '99.9',
+            'cost' => '55',
+            'reorder_point' => '30',
+            'active' => '1',
+            'description' => 'Description for product 9'
+        ]]);
     
     }
 
     public function test_it_deletes_product()
     {
-        $response = $this->delete('api/products/1');
+        $response = $this->get('api/products/search/Product_update');
+        $response->assertStatus(200);
+
+        $response = $this->delete('api/products/'.$response->getData()[0]->id);
         $response->assertStatus(200);
     
     }
